f63acb218de23c68fe3db4a5a59d575a6e744f89
Brooks Johnson <brooks.johnson88@gmail.com>
2017-04-08 18:51:40 -0500

    Connect to the database and get schema as JSON

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
+var mysqlspec = require('mysqlspec')
+const ipc = require('electron').ipcMain
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -47,6 +49,19 @@ app.on('activate', () => {
     createWindow()
   }
 })
+
+ipc.on('db-connect', function(event, data){
+    let config = {
+      host     : data.host,
+      user     : data.user,
+      password : data.pass,
+      database : data.db,
+    }
+
+    mysqlspec(config, function (err, schema) {
+      event.sender.send('connection-made', schema)
+    });
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
